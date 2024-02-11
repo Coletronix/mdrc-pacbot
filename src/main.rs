7d6e6cf9cad3f1d10a2fdd76c67b286018b7bc13
MJE10 <63426573+MJE10@users.noreply.github.com>
2024-02-11 01:37:31 -0500

    23 lines

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,21 @@
 use bevy::prelude::{Mut, Resource, World};
-use bevy_egui::egui::{Ui, WidgetText};
 use bevy_egui::{egui, EguiContexts};
 use std::cell::RefMut;
 
 fn ui_system(mut contexts: EguiContexts, mut world: RefMut<World>) {
     let mut app: Mut<App> = world.resource_mut();
-    egui::Window::new("Pacbot simulation")
-        .show(contexts.ctx_mut(), |f| app.update(contexts.ctx_mut(), f));
-}
-
-#[derive(Default)]
-struct TabViewer {}
-
-impl egui_dock::TabViewer for TabViewer {
-    type Tab = ();
-
-    fn title(&mut self, tab: &mut Self::Tab) -> WidgetText {
-        WidgetText::from("Stopwatch")
-    }
-
-    fn ui(&mut self, ui: &mut Ui, tab: &mut Self::Tab) {}
+    egui::Window::new("Pacbot simulation").show(contexts.ctx_mut(), |f| app.update(f));
 }
 
 pub trait PacbotWidget {
     fn update(&mut self) {}
 }
 
-#[derive(Resource)]
+#[derive(Resource, Default)]
 struct App {}
 
-impl Default for App {
-    fn default() -> Self {
-        Self {}
-    }
-}
 impl App {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+    fn update(&mut self, _frame: &mut eframe::Frame) {}
 }
 
 fn main() {
